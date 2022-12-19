739063fa5af4795f8ab1edcc6b8886b8e1d3c2f5
Marek <marek7119@gmail.com>
2022-12-19 18:28:23 +0100

    move gitignore and apply it to files

---
+++ src/var/cache/prod/smarty/compile/classic/ef/29/23/ef292351cfeda8c9877ef037294c39c8868d6a98_0.file.popup.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.43, created on 2022-12-19 18:21:42
+  from '/var/www/html/modules/welcome/views/templates/popup.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.43',
+  'unifunc' => 'content_63a09da682dd68_71254574',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ef292351cfeda8c9877ef037294c39c8868d6a98' => 
+    array (
+      0 => '/var/www/html/modules/welcome/views/templates/popup.tpl',
+      1 => 1671393355,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63a09da682dd68_71254574 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-popup bootstrap">
+  <div class="content"></div>
+</div>
+<?php }
+}
